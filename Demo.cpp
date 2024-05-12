b9a7c2b9f5a853dc8e6739d49874896e19201ea0
Ido0302 <160231274+Ido0302@users.noreply.github.com>
2024-05-12 18:17:46 +0300

    Add files via upload

--- Demo.cpp
+++ Demo.cpp
@@ -19,7 +19,7 @@ using namespace ariel;
 int main()
 {
     
-    Graph g;
+    Graph g; 
     
     // 3x3 matrix that represents a connected graph.
     vector<vector<int>> graph = {
@@ -86,53 +86,26 @@ int main()
     {
         cout << e.what() << endl; // Should print: "Invalid graph: The graph is not a square matrix."
     }
-    
 
-/*
+
+
 vector<vector<int>> graph5 = {
-        {0, 7, 8, 0, 0},
-        {0, 0, 0, 5, 0},
-        {0, -10, 0, 2, 0},
-        {0, 0, 0, 0, -1},
-        {0, 0, -1, 0, 0}};
-    g.loadGraph(graph5); // Load the graph to the object.
-    g.printGraph();  //Graph with 5 vertices and 9 edges. The graph is directed."
-
-    cout << g.isDirect << endl;        // Should print: "1" (true).
-    cout << Algorithms::isConnected(g) << endl;      // Should print: "1" (true). 
-    Algorithms::shortestPath(g,0,1);  //0-2-1 
-    Algorithms::shortestPath(g,2,3);  //2-1-3
-    Algorithms::shortestPath(g,0,3);  //0-2-1-3
-    Algorithms::shortestPath(g,2,4);  //2-1-3-4
-    Algorithms::shortestPath(g,1,0);  //-1
-    cout << "************" << endl;
-    cout << Algorithms::isContainsCycle(g) << endl;    // Should print: "The cycle is: 0->1->3->4->0".
+    {0,-3,0},
+    {0,0,-5},
+    {-7,0,0}};
+    g.loadGraph(graph5);
+    cout << Algorithms::negativeCycle(g) << endl; //0->1->2->0
+    cout << "*********" << endl;
 
 
     vector<vector<int>> graph6 = {
-        {0,3,0,0},
-        {0,0,-4,8},
-        {0,0,0,-5},
-        {7,-6,0,0},
+        {0, 2, -20, 0},
+        {0, 0, 4, 0},
+        {0, 0, 0, 6},
+        {8, 0, 0, 0}
         };
-    g.loadGraph(graph6);   // Load the graph to the object. 
-    cout << Algorithms::isConnected(g) << endl; 
-    cout << Algorithms::shortestPath(g,2,1) << endl;
-    cout << Algorithms::shortestPath(g,0,2) << endl;
-    cout << Algorithms::isContainsCycle(g) << endl;  
-    Algorithms::negativeCycle(g);
-    cout << "************" << endl;
-      
-
-     vector<vector<int>> graph7 = {
-        {0, 1, 0, 0, 0},
-        {1, 0, 3, 0, 0},
-        {0, 3, 0, 4, 0},
-        {0, 0, 4, 0, 5},
-        {0, 0, 0, 5, 0}};
-    g.loadGraph(graph3);
-    cout << ariel::Algorithms::isBipartite(g) << endl; // "The graph is bipartite: A={0, 2, 4}, B={1, 3}";
-*/
-            
+    g.loadGraph(graph6); // Load the graph to the object.
+    cout << Algorithms::negativeCycle(g) << endl; //0->2->3->0
+          
    return 0;
 }
